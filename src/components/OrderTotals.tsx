2ee035e847e9fc47f2736842a04dddf55c024275
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-03-20 17:37:32 -0300

    Creando el Componente con los totales

---
+++ src/components/OrderTotals.tsx
@@ -0,0 +1,16 @@
+
+const OrderTotals = () => {
+
+  return (
+    <>
+      <div className="space-y-3">
+        <h2 className="font-bold text-3xl">Total y Propinas</h2>
+        <p className="font-bold text-2xl">SubTotal: <span className="font-normal">$0</span></p>
+        <p className="font-bold text-2xl">Propina: <span className="font-normal">$0</span></p>
+        <p className="font-bold text-2xl">Total a Pagar: <span className="font-normal">$0</span></p>
+      </div>
+    </>
+  )
+}
+
+export default OrderTotals
