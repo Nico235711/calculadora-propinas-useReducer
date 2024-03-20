2818d3df4db2dfba4180fbf56f355dd8a76ba839
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-03-20 12:50:41 -0300

    Creando el Header

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ function App() {
 
   return (
     <>
-      <h1>Todo listo</h1>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-4xl font-bold text-center">Calculadora de Propinas y Consumo</h1>
+      </header>
     </>
   )
 }
