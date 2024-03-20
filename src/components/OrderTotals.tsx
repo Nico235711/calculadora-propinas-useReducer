88d4dc58467390e1038090d089d9bf11365aaae7
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-03-20 19:12:26 -0300

    Calculando el Total de la propina

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -4,19 +4,22 @@ import { formatCurrency } from "../helpers"
 
 type OrderTotalsProps = {
   order: OrderItem[]
+  tip: number
 }
 
-const OrderTotals = ({ order }: OrderTotalsProps) => {
+const OrderTotals = ({ order, tip }: OrderTotalsProps) => {
 
   const subTotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
+
+  const tipAmount = useMemo(() => subTotalAmount * tip, [tip, order])
 
   return (
     <>
       <div className="space-y-3">
         <h2 className="font-bold text-3xl">Total y Propinas</h2>
         <p className="font-bold text-2xl">SubTotal: <span className="font-normal">{formatCurrency(subTotalAmount)}</span></p>
-        <p className="font-bold text-2xl">Propina: <span className="font-normal">$0</span></p>
-        <p className="font-bold text-2xl">Total a Pagar: <span className="font-normal">$0</span></p>
+        <p className="font-bold text-2xl">Propina: <span className="font-normal">{formatCurrency(tipAmount)}</span></p>
+        <p className="font-bold text-2xl">Total a Pagar: <span className="font-normal">{formatCurrency(subTotalAmount + tipAmount)}</span></p>
       </div>
     </>
   )
