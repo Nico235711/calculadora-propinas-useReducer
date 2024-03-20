66f182d59ba1b6a3523a88d1d9517f982726f9f4
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-03-20 15:52:01 -0300

    Evitando duplicados

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -6,12 +6,23 @@ const useOrder = () => {
   const [order, setOrder] = useState<OrderItem[]>([])
 
   const addItem = (item: MenuItems) => {
-    
-    // no puedo agregar a order el item porque el item no tiene la propiedad
-    // quantity asi que creo un nuevo item tomando una copia de las propiedades
-    // del item anterior pero con la propiedad nueva 
-    const newItem = { ...item, quantity: 1 } 
-    setOrder([...order, newItem])
+
+    const itemExists = order.find(orderItem => orderItem.id === item.id)
+
+    if (itemExists) { // si existe en mi orden
+      const updatedOrder = order.map(orderItem => (
+        orderItem.id === item.id ? 
+        { ...orderItem, quantity: orderItem.quantity + 1 } : orderItem
+      ))
+      setOrder(updatedOrder)      
+
+    } else { // no existe en mi orden
+      // no puedo agregar a order el item porque el item no tiene la propiedad
+      // quantity asi que creo un nuevo item tomando una copia de las propiedades
+      // del item anterior pero con la propiedad nueva 
+      const newItem = { ...item, quantity: 1 } 
+      setOrder([...order, newItem])
+    }
   }
 
   return {
