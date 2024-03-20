f567c906376e57862fce48df8b6a4f4025517beb
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-03-20 14:49:49 -0300

    Añadiendo elementos al state de Orden

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -5,9 +5,13 @@ const useOrder = () => {
 
   const [order, setOrder] = useState<OrderItem[]>([])
 
-  const addItem = (item: MenuItems) => {  
-    console.log(item);
+  const addItem = (item: MenuItems) => {
     
+    // no puedo agregar a order el item porque el item no tiene la propiedad
+    // quantity asi que creo un nuevo item tomando una copia de las propiedades
+    // del item anterior pero con la propiedad nueva 
+    const newItem = { ...item, quantity: 1 } 
+    setOrder([...order, newItem])
   }
 
   return {
